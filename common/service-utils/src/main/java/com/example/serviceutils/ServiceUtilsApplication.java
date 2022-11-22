b016732a8c5331fd517250f267f07e0fa0aef115
LazyAction <LazyAction@outlook.com>
2022-11-22 17:25:41 +0800

    Initial commit

---
+++ common/service-utils/src/main/java/com/example/serviceutils/ServiceUtilsApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviceutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceUtilsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceUtilsApplication.class, args);
+    }
+
+}
---
+++ common/service-utils/src/test/java/com/example/serviceutils/ServiceUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serviceutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
